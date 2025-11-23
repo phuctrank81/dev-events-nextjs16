2174cdbc0f5c3e590dc2dc71394d0744a681c27e
phuctrank81 <131422071+phuctrank81@users.noreply.github.com>
2025-11-23 15:33:26 +0700

    first commit

---
+++ components/ExploreBtn.tsx
@@ -0,0 +1,15 @@
+'use client'
+import Image from 'next/image'
+
+const ExploreBtn = () => {
+  return (
+    <button type="button" id="explore-btn" className="mt-7 mx-auto" onClick={() => console.log("Explore button clicked")}>
+        <a href="#events">
+            Explore Events
+            <Image src="/icons/arrow-down.svg" alt="arrow-arrow" width={24} height={24} className="inline-block ml-2" />
+        </a>
+    </button>
+  )
+}
+
+export default ExploreBtn
